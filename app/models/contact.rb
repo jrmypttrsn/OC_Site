086b24a1a901c78bc0222ebc96593a30f9b8c245
kylelaughlin <kylejlaughlin@icloud.com>
2016-06-24 13:53:35 -0500

    add contact validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -17,4 +17,9 @@
 #
 
 class Contact < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  
 end
